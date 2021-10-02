ce2c3835095086783479e42bc0f8d38db9bb59d4
Alemnew Marie <alemnewmarie461@gmail.com>
2021-10-02 20:44:30 +0300

    Podt update and delete functionality for authenicated users

--- tomar/apps/blog/urls.py
+++ tomar/apps/blog/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path("", views.index, name="blog-home"),
     path("post/new/", views.PostCreateView.as_view(), name="post-create"),
     path("post/<slug:slug>/", views.PostDetailView.as_view(), name="post-detail"),
+    path(
+        "post/<slug:slug>/update/", views.PostUpdateView.as_view(), name="post-update"
+    ),
+    path(
+        "post/<slug:slug>/delete/", views.PostDeleteView.as_view(), name="post-delete"
+    ),
 ]
--- tomar/apps/blog/views.py
+++ tomar/apps/blog/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.models import User
+from django.http import request
+from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import Post
 
@@ -38,7 +39,38 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
     model = Post
     fields = ["category", "title", "content", "summary", "image"]
+    template_name = "blog/post_create_form.html"
 
     def form_valid(self, form):
         form.instance.user = self.request.user  # assign post author
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Display post update form and handle post update."""
+
+    model = Post
+    fields = ["category", "title", "content", "summary", "image"]
+    template_name = "blog/post_update_form.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user  # assign post author
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.user
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Display post deletion form and handle post deletion."""
+
+    model = Post
+
+    def get_success_url(self):
+        post_slug = self.kwargs["slug"]
+        user = Post.objects.get(slug=post_slug).user
+        return reverse_lazy("account:profile", args=(user.username,))
+
+    def test_func(self):
+        return self.get_object().user == self.request.user
